d4bb10ed82669477ace23299630dc6a080903882
vuongpm <phanvuong118@gmail.com>
2026-03-14 17:15:16 +0700

    Use React hook form and zod to valid LoginPage; use Field, FieldError to display errors

--- src/app/pages/auth/LoginPage.tsx
+++ src/app/pages/auth/LoginPage.tsx
@@ -1,3 +1,7 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,13 +11,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Input, InputPassword } from "@/components/ui/input";
 import { LockSimpleIcon, UserIcon } from "@phosphor-icons/react";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+
+const loginSchema = z.object({
+  username: z.string().min(1, { message: "Vui lòng nhập tên đăng nhập" }),
+  password: z.string().min(1, { message: "Vui lòng nhập mật khẩu" }),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: LoginFormValues) => {
+    //TODO: Call API
+    console.log("Dữ liệu an toàn để gọi API:", values);
+  };
+
   return (
-    <div className="min-h-dvh flex items-center justify-center p-4 sm:p-8 bg-linear-to-br from-[#d1e8fe] via-[#f8fafc] to-[#dfedff]">
+    <div className="min-h-dvh flex items-center justify-center bg-linear-to-br from-[#d1e8fe] via-[#f8fafc] to-[#dfedff]">
       <Card className="w-full max-w-sm shadow-xl border-0 backdrop-blur-sm mt-auto mb-auto">
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Đăng nhập</CardTitle>
@@ -21,23 +54,26 @@ export default function LoginPage() {
             Vui lòng đăng nhập để truy cập hệ thống
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="username">Tên đăng nhập</Label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <FieldGroup>
+              <Field data-invalid={!!errors.username}>
+                <FieldLabel htmlFor="username">Tên đăng nhập</FieldLabel>
                 <Input
                   id="username"
                   type="text"
                   autoComplete="username"
                   placeholder="Nhập tên đăng nhập"
                   icon={<UserIcon weight="fill" />}
-                  required
+                  {...register("username")}
+                  aria-invalid={!!errors.username}
                 />
-              </div>
-              <div className="grid gap-2">
+                {errors.username && <FieldError errors={[errors.username]} />}
+              </Field>
+
+              <Field data-invalid={!!errors.password}>
                 <div className="flex items-center">
-                  <Label htmlFor="password">Mật khẩu</Label>
+                  <FieldLabel htmlFor="password">Mật khẩu</FieldLabel>
                   <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
@@ -45,25 +81,27 @@ export default function LoginPage() {
                     Quên mật khẩu?
                   </a>
                 </div>
-                <Input
+                <InputPassword
                   id="password"
-                  type="password"
                   placeholder="Nhập mật khẩu"
                   icon={<LockSimpleIcon weight="fill" />}
-                  required
+                  {...register("password")}
+                  aria-invalid={!!errors.password}
                 />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2 mt-6">
-          <Button
-            type="submit"
-            className="w-full h-9"
-          >
-            Đăng nhập
-          </Button>
-        </CardFooter>
+                {errors.password && <FieldError errors={[errors.password]} />}
+              </Field>
+            </FieldGroup>
+          </CardContent>
+
+          <CardFooter className="flex-col gap-2 mt-12">
+            <Button
+              type="submit"
+              className="w-full h-9"
+            >
+              Đăng nhập
+            </Button>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );

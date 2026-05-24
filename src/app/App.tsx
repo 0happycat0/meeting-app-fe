fdc1562683d9ac8b905fb5add3b870cfe214c819
vuongpm <phanvuong118@gmail.com>
2026-05-24 11:07:48 +0700

    Refactor: Implement AuthProvider for Keycloak authentication and integrate QueryClientProvider in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,30 @@ import { RouterProvider } from "react-router-dom";
 import "./styles/App.css";
 import { router } from "./router";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import AuthProvider from "./AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5 minutes,
+    },
+  },
+});
 
 function App() {
   return (
-    <TooltipProvider>
-      <RouterProvider router={router} />
-    </TooltipProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster position="bottom-right" theme="light" closeButton/>
+        </AuthProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
